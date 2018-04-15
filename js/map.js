549d333982a18d9fdef727b5a8cd3c2d9b1dea02
Alexander <sitesmake@gmail.com>
2018-04-15 03:41:55 +0300

    Add validations logick

--- js/map.js
+++ js/map.js
@@ -234,3 +234,74 @@ var MapPinMouseUpHandler = function (evt) {
 };
 
 document.addEventListener('mouseup', MapPinMouseUpHandler);
+
+
+var timeinField = document.getElementById('timein');
+var timeoutField = document.getElementById('timeout');
+
+timeinField.addEventListener('change', function () {
+  timeoutField.value = timeinField.value;
+});
+
+timeoutField.addEventListener('change', function () {
+  timeinField.value = timeoutField.value;
+});
+
+var typeField = document.getElementById('type');
+var priceField = document.getElementById('price');
+var typeToMinPricePerNightMapper = {
+  flat: 1000,
+  bungalo: 0,
+  house: 5000,
+  palace: 10000
+};
+
+var updateMinPricePerNight = function () {
+  var currentTypeValue = typeField.value;
+  var newMinPrice = typeToMinPricePerNightMapper[currentTypeValue];
+  priceField.min = newMinPrice;
+  priceField.placeholder = newMinPrice;
+};
+updateMinPricePerNight();
+
+typeField.addEventListener('change', updateMinPricePerNight);
+
+var roomNumberField = document.getElementById('room_number');
+var capacityField = document.getElementById('capacity');
+var roomNumberToCapacityMapper = {
+  '1': ['1'],
+  '2': ['1', '2'],
+  '3': ['1', '2', '3'],
+  '100': ['0']
+};
+
+var generateGuestsMessage = function (guests) {
+  if (guests.includes('0')) {
+    return 'Не для гостей';
+  } else {
+    return 'Допустимое количество гостей: ' + guests.join(', ');
+  }
+};
+
+var checkCapacity = function () {
+  var roomNumber = roomNumberField.value;
+  var currentCapacity = capacityField.value;
+  var allowedCapacity = roomNumberToCapacityMapper[roomNumber];
+  if (allowedCapacity.includes(currentCapacity)) {
+    capacityField.setCustomValidity('');
+  } else {
+    capacityField.setCustomValidity(generateGuestsMessage(allowedCapacity));
+  }
+};
+checkCapacity();
+
+roomNumberField.addEventListener('change', checkCapacity);
+capacityField.addEventListener('change', checkCapacity);
+
+
+// var startup = function () {
+//   setActiveState();
+//   document.getElementById('title').value = 'Тестовые данные для быстрого старта';
+//   document.getElementById('price').value = '9999';
+// };
+// startup();
