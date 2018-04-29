01a99dbed8b1ef9b4b35e0eb2c157022bb02a4fb
Alexander <sitesmake@gmail.com>
2018-04-29 20:24:59 +0300

    Fix comments

--- js/backend.js
+++ js/backend.js
@@ -4,56 +4,43 @@
   var LOAD_URL = 'https://js.dump.academy/keksobooking/data';
   var SEND_URL = 'https://js.dump.academy/keksobooking';
 
+  var request = function (url, method, onSuccess, onFailure, data) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onSuccess(xhr.response);
+      } else {
+        onFailure('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onFailure('Произошла ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onFailure('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = 5000;
+
+    xhr.open(method, url);
+    if (data) {
+      xhr.send(data);
+    } else {
+      xhr.send();
+    }
+  };
+
   window.backend = {
     load: function (onLoad, onError) {
-      var xhr = new XMLHttpRequest();
-      xhr.responseType = 'json';
-
-      xhr.addEventListener('load', function () {
-        if (xhr.status === 200) {
-          onLoad(xhr.response);
-        } else {
-          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-        }
-      });
-
-      xhr.addEventListener('error', function () {
-        onError('Произошла ошибка соединения');
-      });
-
-      xhr.addEventListener('timeout', function () {
-        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-      });
-
-      xhr.timeout = 5000;
-
-      xhr.open('GET', LOAD_URL);
-      xhr.send();
+      request(LOAD_URL, 'GET', onLoad, onError);
     },
+
     save: function (data, onLoad, onError) {
-      var xhr = new XMLHttpRequest();
-      xhr.responseType = 'json';
-
-      xhr.addEventListener('load', function () {
-        if (xhr.status === 200) {
-          onLoad(xhr.response);
-        } else {
-          onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
-        }
-      });
-
-      xhr.addEventListener('error', function () {
-        onError('Произошла ошибка соединения');
-      });
-
-      xhr.addEventListener('timeout', function () {
-        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-      });
-
-      xhr.timeout = 5000;
-
-      xhr.open('POST', SEND_URL);
-      xhr.send(data);
+      request(SEND_URL, 'POST', onLoad, onError, data);
     }
   };
 })();
--- js/utils.js
+++ js/utils.js
@@ -5,17 +5,21 @@
     randomItem: function (items) {
       return items[Math.floor(Math.random() * items.length)];
     },
+
     randomNumber: function (min, max) {
       return min + Math.round(Math.random() * (max - min));
     },
+
     randomSortArray: function (ary) {
       return ary.sort(function () {
         return 0.5 - Math.random();
       });
     },
+
     isOutOfBox: function (container, element, position) {
       return ((position.left < 0) || (position.top < 0) || (position.left + element.width > container.width) || (position.top + element.height > container.height));
     },
+
     showError: function (message) {
       var errorNode = document.createTextNode(message);
       document.body.prepend(errorNode);
